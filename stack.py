efc86b527e6520f8d46972e05f67a648f4e465bc
Tharun_kumar_S <tharunkumar7783@gmail.com>
2026-06-07 22:37:45 +0530

    Day3

--- stack.py
+++ stack.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import yfinance as yf
 
 app = Flask(__name__)
 
@@ -11,11 +12,24 @@ def home():
 def search():
 
     stock = request.form["stock"]
-    return render_template(
-        "index.html",
-        stock=stock
-    )
+
+    try:
+        ticker=yf.Ticker(stock)
+        info=ticker.info
+
+        name=info.get("longName","Not Available")
+        symbol=info.get("symbol",stock)
+        price=info.get("currentPrice","Not Available")
+        asset_type=info.get("quoteType","Note available")
+
+
+        return render_template("index.html",stock=stock,name=name,symbol=symbol,price=price,asset_type=asset_type)
+    except Exception:
+        return render_template("index.html",error="Invalid symbol or no Data")
+    
+    
+
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5002)
