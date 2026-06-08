51f0515349017a178789f1785c8a08a2d7a788a9
Tharun_kumar_S <tharunkumar7783@gmail.com>
2026-06-08 19:58:12 +0530

    Day 4

--- stack.py
+++ stack.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import yfinance as yf
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -11,23 +12,52 @@ def home():
 @app.route("/search", methods=["POST"])
 def search():
 
-    stock = request.form["stock"]
+    stock_df=pd.read_csv("stocks.csv")
+    stock = request.form["stock"].strip()
+    search_item=stock.lower()
+    matched=stock_df[stock_df["name"].str.lower()==search_item]
+
+    if not matched.empty:
+        stock = matched.iloc[0]["ticker"]
 
     try:
         ticker=yf.Ticker(stock)
         info=ticker.info
 
-        name=info.get("longName","Not Available")
-        symbol=info.get("symbol",stock)
-        price=info.get("currentPrice","Not Available")
-        asset_type=info.get("quoteType","Note available")
+        name = (
+            info.get("longName")
+            or info.get("shortName")
+            or "Not Available"
+        )
 
+        symbol = (
+            info.get("symbol")
+            or stock
+        )
 
-        return render_template("index.html",stock=stock,name=name,symbol=symbol,price=price,asset_type=asset_type)
+        price = (
+            info.get("currentPrice")
+            or info.get("regularMarketPrice")
+            or "Not Available")
+
+        asset_type = (
+                info.get("quoteType")
+                or "Not Available"
+                )
+        explanation=info.get("longBusinessSummary","No Data Available")
+        explanation=explanation[:500]+"..."
+
+    
+        return render_template("index.html",stock=stock,name=name,symbol=symbol,price=price,asset_type=asset_type,
+                               explanation=explanation)
+
+    
+    
     except Exception:
         return render_template("index.html",error="Invalid symbol or no Data")
     
     
+
 
 
 
