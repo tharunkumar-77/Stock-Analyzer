7ce1e8fe648b874b5a0b51fd67a7d17260326e85
Tharun_kumar_S <tharunkumar7783@gmail.com>
2026-06-14 19:57:45 +0530

    reborn

--- app.py
+++ app.py
@@ -4,8 +4,95 @@ import pandas as pd
 
 app=Flask(__name__)
 
+def calculate_return(history):
+    if history.empty:
+        return "Not available"
+    
+    start_price=history["Close"].iloc[0]
+    end_price=history["Close"].iloc[-1]
+
+    return ((end_price -start_price) / start_price) *100
+
 
 def get_detail(stock):
+    stock_df = pd.read_csv("stocks.csv")
+    stock = stock.strip()
+
+    search_item = stock.lower()
+
+    matched = None
+
+    for _, row in stock_df.iterrows():
+        if row["name"].lower() == search_item:
+            matched = row
+            break
+
+    if matched is not None:
+        stock = matched["ticker"]
+
+    try:
+        ticker=yf.Ticker(stock)
+        info=ticker.info
+
+        name=info.get("longName") or info.get("shortName") or "Not Available"
+
+        symbol=info.get("symbol") or "Not Available"
+
+        price=info.get("currentPrice") or info.get("regularMarketPrice") or "Not Available"
+
+        asset_type=info.get("quoteType") or "Not Available"
+
+        explanation=info.get("longBusinessSummary") or "Not Available"
+        if explanation != "Not Available":
+            explanation=explanation[:10] +"..."
+
+        history_1y=ticker.history(period="1y")
+        history_3y=ticker.history(period="3y")
+        history_5y=ticker.history(period="5y")
+
+        returns_1y=round(calculate_return(history_1y),2)
+        returns_3y=round(calculate_return(history_3y),2)
+        returns_5y=round(calculate_return(history_5y),2)
+
+        return {
+    "name": name,
+    "symbol": symbol,
+    "price": price,
+    "asset_type":asset_type,
+    "explanation": explanation,
+    "return_1y": returns_1y,
+    "return_3y": returns_3y,
+    "return_5y": returns_5y,
+    }
+
+
+
+    except Exception:
+        return {"error":"Invalid Symbol -_-"}
+    
+
+@app.route("/")
+def home():
+    
+    return render_template("home.html")
+
+@app.route("/search",methods=["POST"])
+def search():
+    stock = request.form["stock"]
+    details = get_detail(stock)
+    return render_template("home.html", **details)
+
+
+@app.route("/historical_calculator",methods=["POST"])
+def historical_calulator():
+    return
+
+if __name__ == "__main__":
+    app.run(debug=True,port=5001)
+
+
+
+
 
     
 
